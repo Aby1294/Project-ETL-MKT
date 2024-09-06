431a2b59b04c5480e5c1a701bd1d95096502b1f9
Aby1294 <carlos.palmaorue@davinci.edu.ar>
2024-09-06 20:31:33 -0300

    Añadir carpeta dbt al proyecto

---
+++ dbt-cloud-mkt-elt-dev/models/silver/twitter_ads_slvr copy 6.sql
@@ -0,0 +1,28 @@
+SELECT *,
+upper(
+  concat(
+    replace(Company," ",""),"_",
+    case
+      when Campaign_Type = 'Email' then 'EM'
+      when Campaign_Type = 'Influencer' then 'INF'
+      when Campaign_Type = 'Display' then 'DSP'
+      when Campaign_Type = 'Search' then 'SRCH'
+      when Campaign_Type = 'Social Media' then 'SM'
+      end, '_',
+      replace(replace(replace(Target_Audience, 'Woman','W-'),'Men', 'M-'), 'All Ages', 'A-A'),'_',
+      case
+        when Location =  'New York' then 'NY'
+        when Location =  'Miami' then 'MIA'
+        when Location =  'Boston' then 'BOS'
+        when Location =  'Chicago' then 'CHI'
+        when Location =  'Seattle' then 'SEA'
+        when Location =  'Las Vegas' then 'LV'
+        when Location =  'Washington' then 'WDC'
+        when Location =  'Nueva Orleans' then 'NO'
+        when Location =  'San Francisco' then 'SF'
+        when Location =  'Los Angeles' then 'LA'
+        end
+  )
+) as Campaign_Name,
+DATE_ADD(Date, INTERVAL cast(replace(Duration, 'days', '')as INT64)DAY) as Date_End
+from {{source("raw","twitter_ads_raw")}}
